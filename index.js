bff6e707e00dcd3e7310a6bc3ff1082083259461
darkoverlordofdata <darkoverlordofdata@gmail.com>
2014-03-09 22:14:21 -0700

    purge

--- index.js
+++
@@ -1,6 +0,0 @@
-require('coffee-script');
-var muninn = require('muninn');
-muninn.init(__dirname);
-var Server = muninn.core.Server;
-(new Server()).start();
-
--- src/models/WineCollection.coffee
+++
@@ -1,5 +0,0 @@
-models = require('../models')
-
-class models.WineCollection extends Backbone.Collection
-  model: models.Wine
-  url: "/wines"
